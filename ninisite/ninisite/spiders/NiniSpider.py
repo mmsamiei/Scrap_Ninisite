671efee41fbdfab0e49381a67d9d96a58f1cf75c
mmsamiei <mohmahsamiei@gmail.com>
2018-06-03 22:34:18 +0430

    now we detect category pages that are old

--- ninisite/ninisite/spiders/NiniSpider.py
+++ ninisite/ninisite/spiders/NiniSpider.py
@@ -1,8 +1,15 @@
 import scrapy
+from pymongo import MongoClient
+from scrapy.conf import settings
 
 
 class NiniSpider(scrapy.Spider):
     name = "NiniSpider"
+
+    def __init__(self):
+        connection = MongoClient(settings['MONGODB_SERVER'], settings['MONGODB_PORT'])
+        db = connection[settings['MONGODB_DB']]
+        self.collection = db[settings['MONGODB_COLLECTION']]
 
     def start_requests(self):
         urls = [
@@ -22,13 +29,16 @@ class NiniSpider(scrapy.Spider):
 
     def parse_category_page(self, response):
         topic_links = response.xpath('//*[contains(@class, "topic--title")]/../@href').extract()
-        for topic_link in topic_links:
-            topic_absolute_link = response.urljoin(topic_link)
-            yield scrapy.Request(url=topic_absolute_link, callback=self.parse_topic_page)
+        first_topic_absolute_link = response.urljoin(topic_links[0])
+        is_old = self.collection.find({'url':first_topic_absolute_link}).count()
+        if is_old != 0:
+            for topic_link in topic_links:
+                topic_absolute_link = response.urljoin(topic_link)
+                yield scrapy.Request(url=topic_absolute_link, callback=self.parse_topic_page)
+                #TODO go to next page!
 
     def parse_topic_page(self, response):
         topic_title = response.xpath('//*[contains(@class, "topic-title")]/a/text()').extract_first() 
-        #topic_message = response.xpath('//*[contains(@class, "post-message")]/p/text()').extract_first()
         main_post_message = response.xpath('//*[contains(@class, "post-message")]')[0]
         main_post_message_text = ' '.join(main_post_message.xpath('./p/text()').extract())
         topic_url = response.request.url
